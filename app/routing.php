fafe0d7b511e7bd772eba40d620af0c837b7ccc6
Julien <julien.alexandre26@gmail.com>
2018-04-10 15:50:31 +0200

    Intégration de la page d'accueil avec la navbar et le footer

--- app/routing.php
+++ app/routing.php
@@ -10,10 +10,7 @@
  */
 
 $routes = [
-    'Item' => [ // Controller
+    'Index' => [ // Controller
         ['index', '/', 'GET'], // action, url, method
-        ['add', '/item/add', 'GET'], // action, url, method
-        ['edit', '/item/edit/{id:\d+}', 'GET'], // action, url, method
-        ['show', '/item/{id:\d+}', 'GET'], // action, url, method
     ],
 ];
